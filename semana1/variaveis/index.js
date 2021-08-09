d1cc55a99d92a5c34d1938fcaa9d9048c4c12ebd
Leticia Agante de Brito <leticia.nos@gmail.com>
2021-08-09 16:28:04 -0300

    variaveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,54 @@
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)*/
+
+// o resultado vai ser 10 5, pois no b foi mudado no final, então ele vai imprimir a mudança.
+
+/*let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) */
+
+// ele mostra 10 20 c, pois ele só endende letras quando estão em "".
+
+/*let p = prompt("Quantas horas você trabalha por dia?") //horasTrabalhadas
+let t = prompt("Quanto você recebe por dia?") //dinheiroPorDia
+alert(`Voce recebe ${t/p} por hora`) */
+
+//EXERCICIOS DE ESCRITA DE CODIGO
+
+/*const seuNome 
+let idade
+
+console.log(typeof seuNome) 
+console.log(typeof idade) */
+
+// essa mensagem apareceu porque não se deu uma valor.
+
+
+/*const seuNome = prompt("Qual o seu nome? ")
+const idade = prompt("Qual a sua idade? ")
+
+console.log(seuNome)//string
+console.log(idade)//number
+//por conta de eu ter dado valor as variaveis, ele retornou 
+
+console.log("Meu nome é ", seuNome, "Eu tenho ", idade, "anos" )*/
+
+//EXERCICIO 2
+
+/*const camisetaPreta = false
+const calcas = true
+const meia = true 
+
+console.log("Você esta de camiseta preta? ", camisetaPreta)
+console.log("Você está usando calças? ", calcas)
+console.log("Você esta de meia? ", meia)*/
+
